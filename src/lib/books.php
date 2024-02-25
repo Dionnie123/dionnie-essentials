e626f84fdba3d99d17358923b0c3ea1ba1a4523d
Mark Dionnie Bulingit <dionnie_bulingit@yahoo.com>
2024-02-25 17:42:05 +0800

    up

--- src/lib/books.php
+++ src/lib/books.php
@@ -45,6 +45,12 @@ class BookDetailMetaBox
 
     public function __construct()
     {
+        /*
+        https://www.reddit.com/r/Wordpress/comments/8hvh0y/difference_in_meta_keys_with_and_without/
+        
+        I think if you use an underscore prefix it is supposed to be used by plugins and themes 
+        and don't appear in the UI below the post/page i.e. are created programatically.
+        */
         $this->author = _pluginname_private_metakey("_pluginname_book_author");
         $this->published_date = _pluginname_private_metakey("_pluginname_book_published_date");
         $this->layout = _pluginname_private_metakey("_pluginname_book_layout");
@@ -78,20 +84,17 @@ class BookDetailMetaBox
 
         wp_nonce_field($this->nonce_action, $this->nonce_field);
 ?>
-<label for="<?php echo $this->author; ?>">Author:</label>
-<input class="widefat" type="text" id="<?php echo $this->author; ?>" name="<?php echo $this->author; ?>"
-    value="<?php echo esc_attr($author); ?>" />
+        <label for="<?php echo $this->author; ?>">Author:</label>
+        <input class="widefat" type="text" id="<?php echo $this->author; ?>" name="<?php echo $this->author; ?>" value="<?php echo esc_attr($author); ?>" />
 
-<label for="<?php echo $this->published_date; ?>">Published Date:</label>
-<input class="widefat" type="text" id="<?php echo $this->published_date; ?>" name="<?php echo $this->published_date; ?>"
-    value="<?php echo esc_attr($published_date); ?>" />
+        <label for="<?php echo $this->published_date; ?>">Published Date:</label>
+        <input class="widefat" type="text" id="<?php echo $this->published_date; ?>" name="<?php echo $this->published_date; ?>" value="<?php echo esc_attr($published_date); ?>" />
 
-<label for="<?php echo $this->layout; ?>">Layout:</label>
-<select class="widefat" name="<?php echo $this->layout; ?>" id="<?php echo $this->layout; ?>"
-    value="<?php echo esc_attr($layout); ?>">
-    <option <?php selected($layout, "full") ?> value="full">Full Width</option>
-    <option <?php selected($layout, "sidebar") ?> value="sidebar">Post with Sidebar</option>
-</select>
+        <label for="<?php echo $this->layout; ?>">Layout:</label>
+        <select class="widefat" name="<?php echo $this->layout; ?>" id="<?php echo $this->layout; ?>" value="<?php echo esc_attr($layout); ?>">
+            <option <?php selected($layout, "full") ?> value="full">Full Width</option>
+            <option <?php selected($layout, "sidebar") ?> value="sidebar">Post with Sidebar</option>
+        </select>
 
 <?php
     }
@@ -119,6 +122,7 @@ class BookDetailMetaBox
 
 
         // Reason why we put '_' on this case is metakeys need to have underscore as prefix
+        //
 
         update_post_meta($post_id, $this->author, $author);
         update_post_meta($post_id, $this->published_date, $published_date);
